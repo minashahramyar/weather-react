e00fa1e2662b1f58d41311270e738a402dc172cd
minayar88 <122382410+minayar88@users.noreply.github.com>
2024-02-18 22:14:11 +0000

    Weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import react, { useState } from "react";
+import axios from "axios";
+import { Audio } from "react-loader-spinner";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React now
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const [city, setCity] = useState(null);
+  const [temp, setTemp] = useState(null);
+  const [desc, setDesc] = useState(null);
+  const [humidity, setHumidity] = useState(null);
+  const [wind, setWind] = useState(null);
+  function showTemp(response) {
+    console.log(response);
+    setTemp(Math.round(response.data.main.temp));
+    setDesc(response.data.weather[0].description);
+    setHumidity(Math.round(response.data.main.humidity));
+    setWind(Math.round(response.data.wind.speed));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2e3e5873dd1cc627eb6ff6bc0d7327f5&units=metric`;
+    axios.get(url).then(showTemp);
+  }
+
+  function updateCity(event) {
+    let city = event.target.value;
+    setCity(city);
+  }
+
+  if (temp) {
+    return (
+      <div className="App">
+        <h1>Weather App</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="text" onChange={updateCity} placeholder="Enter a city" />
+          <input type="submit" value="Search" />
+        </form>
+        <h3>Temperature: {temp}</h3>
+        <h3>Description: {desc}</h3>
+        <h3>Humidity: {humidity}</h3>
+        <h3>Wind: {wind}</h3>
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <h1>Weather App</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="text" onChange={updateCity} placeholder="Enter a city" />
+          <input type="submit" value="Search" />
+        </form>
+        <Audio
+          height="80"
+          width="80"
+          radius="9"
+          color="green"
+          ariaLabel="loading"
+          wrapperStyle
+          wrapperClass
+        />
+      </div>
+    );
+  }
 }
-
-export default App;
